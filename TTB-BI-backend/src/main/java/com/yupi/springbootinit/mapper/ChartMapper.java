f30c0504aa0d912bb67ef774000ba4fde7ee2720
Wgz <1808365012@mail.ynu.edu.cn>
2023-08-23 16:50:47 +0800

    基本框架

---
+++ TTB-BI-backend/src/main/java/com/yupi/springbootinit/mapper/ChartMapper.java
@@ -0,0 +1,18 @@
+package com.yupi.springbootinit.mapper;
+
+import com.yupi.springbootinit.model.entity.Chart;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author 下水道的小老鼠
+* @description 针对表【chart(图表信息表)】的数据库操作Mapper
+* @createDate 2023-08-23 15:17:57
+* @Entity com.yupi.springbootinit.model.entity.Chart
+*/
+public interface ChartMapper extends BaseMapper<Chart> {
+
+}
+
+
+
+
---
+++ TTB-BI-backend/src/main/java/com/yupi/springbootinit/model/entity/Chart.java
@@ -0,0 +1,80 @@
+package com.yupi.springbootinit.model.entity;
+
+import com.baomidou.mybatisplus.annotation.*;
+
+import java.io.Serializable;
+import java.util.Date;
+import lombok.Data;
+
+/**
+ * 图表信息表
+ * @author 下水道的小老鼠
+ * @TableName chart
+ */
+@TableName(value ="chart")
+@Data
+public class Chart implements Serializable {
+    /**
+     * id
+     */
+    @TableId(value = "id", type = IdType.ASSIGN_ID)
+    private Long id;
+
+    /**
+     * 分析目标
+     */
+    @TableField(value = "goal")
+    private String goal;
+
+    /**
+     * 图表数据
+     */
+    @TableField(value = "chartData")
+    private String chartData;
+
+    /**
+     * 图表类型
+     */
+    @TableField(value = "chartType")
+    private String chartType;
+
+    /**
+     * 生成的图表数据
+     */
+    @TableField(value = "genChart")
+    private String genChart;
+
+    /**
+     * 生成的分析结论
+     */
+    @TableField(value = "genResult")
+    private String genResult;
+
+    /**
+     * 创建用户 id
+     */
+    @TableField(value = "userId")
+    private Long userId;
+
+    /**
+     * 创建时间
+     */
+    @TableField(value = "createTime")
+    private Date createTime;
+
+    /**
+     * 更新时间
+     */
+    @TableField(value = "updateTime")
+    private Date updateTime;
+
+    /**
+     * 是否删除
+     */
+    @TableLogic
+    @TableField(value = "isDelete")
+    private Integer isDelete;
+
+    @TableField(exist = false)
+    private static final long serialVersionUID = 1L;
+}
