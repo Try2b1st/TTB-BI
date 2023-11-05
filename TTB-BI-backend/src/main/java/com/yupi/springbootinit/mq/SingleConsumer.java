63bc4131772bbd510fff114600df37a25718264f
Wgz <1808365012@mail.ynu.edu.cn>
2023-11-05 17:01:08 +0800

    学习消息队列，一个生产者对一个消费者

---
+++ TTB-BI-backend/src/main/java/com/yupi/springbootinit/mq/SingleConsumer.java
@@ -0,0 +1,34 @@
+package com.yupi.springbootinit.mq;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+import java.nio.charset.StandardCharsets;
+
+public class SingleConsumer {
+
+    private final static String QUEUE_NAME = "hello";
+
+    public static void main(String[] argv) throws Exception {
+        //建立连接工厂
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+
+        //获取连接
+        Connection connection = factory.newConnection();
+        //建立频道
+        Channel channel = connection.createChannel();
+
+        //声明队列
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            //持续消费消息，如果消息队列中有消息
+            String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
+            System.out.println(" [x] Received '" + message + "'");
+        };
+        channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
+    }
+}
---
+++ TTB-BI-backend/src/main/java/com/yupi/springbootinit/mq/SingleProducer.java
@@ -0,0 +1,31 @@
+package com.yupi.springbootinit.mq;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.nio.charset.StandardCharsets;
+
+public class SingleProducer {
+
+    private final static String QUEUE_NAME = "hello";
+
+    public static void main(String[] argv) throws Exception {
+        //建立连接工厂
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+//        factory.setUsername();
+//        factory.setPassword();
+
+        //创建操作RabbitMQ的客户端
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+            //建立队列
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            String message = "我是你大爷!";
+            //发送消息
+            channel.basicPublish("", QUEUE_NAME, null, message.getBytes(StandardCharsets.UTF_8));
+            System.out.println(" [x] Sent '" + message + "'");
+        }
+    }
+}
